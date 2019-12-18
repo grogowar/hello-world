9c44f390995d1a44d2bb1701e6457e37627ea158
Shuranov Artur <shuranov-a@speechpro.com>
2019-12-18 20:45:54 +0000

    adsf

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "git@github.com:grogowar/hello-world.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/hello-world"
+# set :deploy_to, "/var/www/hello-world"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
